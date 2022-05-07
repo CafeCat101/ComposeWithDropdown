fe8918a93d210997fbf19d180afa126103083a59
Leonore <leonore.yardimli@icloud.com>
2022-05-07 18:47:27 +0800

    Initial Commit

---
+++ ComposeWithDropdown/ComposeWithDropdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposeWithDropdownApp.swift
+//  ComposeWithDropdown
+//
+//  Created by Leonore Yardimli on 2022/5/7.
+//
+
+import SwiftUI
+
+@main
+struct ComposeWithDropdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposeWithDropdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComposeWithDropdown
+//
+//  Created by Leonore Yardimli on 2022/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
